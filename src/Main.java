ff8ef0617636d514d9f1257f9cb83cb25ae84eb2
olhafeidak <62849241+olhafeidak@users.noreply.github.com>
2023-03-02 19:56:23 +0200

    №10

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,17 @@
+public class Main{
 
-    /*Створити програму для виведення чисел від 1 до 10
+        public static void main(String[]args){
 
-            Показати всі парні числа в діапазоні від 1 до 100
+        int length=15;
 
-            Знайти факторіал числа 7
+        int[]array=ArrayGenerator.fillArray(length);
+        System.out.println("Arraywithrandom"+length+"numbers");
+        ArrayGenerator.sortAsc(length,array);
+        System.out.println("SortedarrayAsc:");
+        ArrayGenerator.showArray(length,array);
+        ArrayGenerator.sortDesc(length,array);
+        System.out.println("SortedarrayDesc:");
+        ArrayGenerator.showArray(length,array);
 
-            Вивести числа фібоначчі до 100 (значення самого числа не повинно перевищувати 100)*/
-
-
-    import java.util.Scanner;
-
-    public class Main {
-
-    public static void main(String[] args) {
-
-        //Створити програму для виведення чисел від 1 до 10
-        System.out.println("Number line is from 1 to 10 is: ");
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
         }
-        System.out.println();
-
-        //Показати всі парні числа в діапазоні від 1 до 100
-        int b = 0;
-        System.out.println();
-        System.out.println("Even numbers up to 100 are: ");
-        while (b <= 100) {
-            if (b % 2 == 0) {
-                System.out.print((b + " "));
-            }
-            b++;
-        }
-        System.out.println();
-
-        //Знайти факторіал числа 7
-        int i, fact = 1;
-        int number = 7;
-
-        for (i = 1; i <= number; i++) {
-            fact = fact * i;
-        }
-        System.out.println();
-        System.out.println("Factorial of " + number + " is: " + fact);
-
-        // Вивести числа фібоначчі до 100 (значення самого числа не повинно перевищувати 100)
-
-        System.out.println();
-        System.out.println("Fibonacci task ");
-
-        int f1 = 0;
-        int f2 = 1;
-        int f = 0;
-        while (f < 100) {
-            System.out.print(f + " ");
-            f = f1 + f2;
-            f1 = f2;
-            f2 = f;
-        }
-        System.out.println();
-
-    }
 }
